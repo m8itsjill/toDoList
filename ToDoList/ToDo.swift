ef057c2baed2432a4623bf3a925b8333232d9c25
haileysmallwood <hpkpixie@bellsouth.net>
2019-07-15 10:15:42 -0700

    first save

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,20 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 HS. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+var name = ""
+    var important = false
+        
+}
+
+
+
+
+
